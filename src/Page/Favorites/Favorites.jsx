3d47285c5581b4d667ebe26b9145ab7ffb337413
rabby-web <rabby.webdeveloper@gmail.com>
2023-09-24 00:50:31 +0600

    add to favorite

--- src/Page/Favorites/Favorites.jsx
+++ src/Page/Favorites/Favorites.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+import PhoneCard from "../PhoneCart/PhoneCard";
+
 const Favorites = () => {
+  const [favorites, setFavorites] = useState([]);
+  const [noFound, setNoFound] = useState(false);
+
+  useEffect(() => {
+    const favoriteItems = JSON.parse(localStorage.getItem("favorites"));
+    if (favoriteItems) {
+      setFavorites(favoriteItems);
+    } else {
+      setNoFound("No Data Found");
+    }
+  }, []);
+  console.log(favorites);
   return (
     <div>
-      <h2>Favorites</h2>
+      {noFound ? (
+        <p>Phones Add To Favorites</p>
+      ) : (
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          {favorites.map((phone) => (
+            <PhoneCard key={phone.id} phone={phone}></PhoneCard>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
